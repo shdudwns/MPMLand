78e672a101b7a990248e3c0fca0a5efa107f95f1
solo5star <solo_5@naver.com>
2017-11-05 02:47:25 +0900

    Update Sphere.php

--- src/mpm/Sphere.php
+++ src/mpm/Sphere.php
@@ -12,7 +12,7 @@ class Sphere {
 		
 		for($x = $originX - $radius, $maxX = $originX + $radius; $x <= $maxX; $x++){
 			for($y = $originY - $radius, $maxY = $originY + $radius; $y <= $maxY; $y++){
-				for($z = $originZ - $radius, $maxZ = $originZ - $radius; $z <= $maxZ; $z++){
+				for($z = $originZ - $radius, $maxZ = $originZ + $radius; $z <= $maxZ; $z++){
 					if((($originX - $x) ** 2) + (($originY - $y) ** 2) + (($originZ - $z) ** 2) < $radiusSquared){
 						$ret[] = [$x, $y, $z];
 					}
