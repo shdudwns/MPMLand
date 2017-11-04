32009f88f2135ec6423a238da74bfa4a1a5cf449
solo5star <solo_5@naver.com>
2017-11-05 03:21:05 +0900

    refactor

--- src/mpm/Sphere.php
+++ src/mpm/Sphere.php
@@ -7,19 +7,19 @@ use mpm\Sphere;
 class Sphere {
 	
 	public static function getElements(int $originX, int $originY, int $originZ, int $radius) : array{
-		$ret = [];
+		$sphereSerialized = [];
 		$radiusSquared = ($radius ** 2) * 3;
 		
 		for($x = $originX - $radius, $maxX = $originX + $radius; $x <= $maxX; $x++){
 			for($y = $originY - $radius, $maxY = $originY + $radius; $y <= $maxY; $y++){
 				for($z = $originZ - $radius, $maxZ = $originZ + $radius; $z <= $maxZ; $z++){
 					if((($originX - $x) ** 2) + (($originY - $y) ** 2) + (($originZ - $z) ** 2) < $radiusSquared){
-						$ret[] = [$x, $y, $z];
+						$sphereSerialized[] = [$x, $y, $z];
 					}
 				}
 			}
 		}
-		return $ret;
+		return $sphereSerialized;
 	}
 }
 
