0452f7066941959a5a606edf31dfdffd0f1d4471
jun04 <jun040329@gmail.com>
2017-11-09 22:26:13 +0900

    리소스팩

--- src/mpm/IsLandMain.php
+++ src/mpm/IsLandMain.php
@@ -70,7 +70,36 @@ class IsLandMain extends PluginBase implements Listener{
     }
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-
+    if(!file_exists($this->getDataFolder())){
+			mkdir($this->getDataFolder());
+		}
+        $downRP = false;
+        if(!file_exists($this->getDataFolder() . "sspe12_2.mcpack")) {
+            $downRP = true;
+            echo TextFormat::toANSI("§l§f[§bMPMLand§f] 리소스팩 다운중");
+            file_put_contents($this->getDataFolder() . "sspe12_2.mcpack", Utils::getURL("http://ryfol.weebly.com/uploads/4/7/6/5/47651895/sspe13_1.mcpack"));
+        }
+        echo str_repeat("\010", $downRP ? strlen(TextFormat::toANSI("§l§f[§bMPMLand§f] 리소스팩 다운중")) : 0) . TextFormat::toANSI("§f[Spooky] ⚪ Applying resource pack...   "); // Replacing latest message
+        $pack = new ZippedResourcePack($this->getDataFolder() . "sspe12_2.mcpack");
+        $r = new \ReflectionClass("pocketmine\\resourcepacks\\ResourcePackManager");
+        // Reflection because devs thought it was a great idea to not let plugins manage resource packs :/
+        $resourcePacks = $r->getProperty("resourcePacks");
+        $resourcePacks->setAccessible(true);
+        $rps = $resourcePacks->getValue($this->getServer()->getResourceManager());
+        $rps[] = $pack;
+        $resourcePacks->setValue($this->getServer()->getResourceManager(), $rps);
+        $resourceUuids = $r->getProperty("uuidList");
+        $resourceUuids->setAccessible(true);
+        $uuids = $resourceUuids->getValue($this->getServer()->getResourceManager());
+        $uuids[$pack->getPackId()] = $pack;
+        $resourceUuids->setValue($this->getServer()->getResourceManager(), $uuids);
+        // Forcing resource packs. We want the client to hear the music!
+        $forceResources = $r->getProperty("serverForceResources");
+        $forceResources->setAccessible(true);
+        $forceResources->setValue($this->getServer()->getResourceManager(), true);
+        echo str_repeat("\010", strlen("⚪ Applying resource pack... ")) . TextFormat::toANSI("§a✔️ 리소스팩 로드완료    \n");
+    }
+}
     if($this->s['island']['make']){
 		Generator::addGenerator(LandGenerator::class, "island");
 		$gener = Generator::getGenerator("island");
